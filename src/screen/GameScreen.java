7e732fbd9835206359c4a51c97af7810a67cecf2
E Stringham <backfrip@users.noreply.github.com>
2015-05-28 23:56:06 -0400

    Resource addition

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -12,17 +12,10 @@ import main.*;
 public class GameScreen implements Screen {
     private OurGame game;
     private OrthographicCamera camera;
-    private CharSequence str;
-    private SpriteBatch spriteBatch;
-    private BitmapFont font;
-    private int counter;
 
     public GameScreen(OurGame gameRef) {
 	game = gameRef;
 	camera = new OrthographicCamera();
-	spriteBatch = new SpriteBatch();
-	font = new BitmapFont();
-	counter = 0;
     }
 
     @Override
@@ -48,15 +41,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-	Gdx.gl.glClearColor(0, 0.5f, 0.5f, 1);
+	Gdx.gl.glClearColor(1, 1, 1, 1);
 	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-	str = Integer.toString(counter);
-	counter++;
-
-	spriteBatch.begin();
-	font.draw(spriteBatch, str, 10, 470);
-	spriteBatch.end();
     }
 
     @Override
