08d9526ee92974a6ea13ea51ef3534130491af1d
Wayez <c.wayez3793@gmail.com>
2015-05-28 23:38:42 -0400

    Playing around with the stats

--- src/objects/Player.java
+++ src/objects/Player.java
@@ -6,6 +6,7 @@ public class Player {
 	private double xcor, ycor;
 	private final String name, type;
 	private ArrayList<Integer> stats;
+	//private Item heldItem;
 	public Player(String name){
 		this(name, "default");
 	}
@@ -15,6 +16,8 @@ public class Player {
 		ycor = 0;
 		this.name = name;
 		this.type = type;
+		stats = new ArrayList<Integer>(10);
+		setBaseStats();
 	}
 	public int getLevel(){
 		//Calculation to get level
@@ -41,5 +44,49 @@ public class Player {
 	public String getType(){
 		return type;
 	}
-	
+	public void setBaseStats(){
+		//Will be determined by Player type but putting a default for now
+		stats.set(0, 10); //Max HP
+		stats.set(1, 10); //Current HP
+		stats.set(2, 10); //Max Source of power
+		stats.set(3, 10); //Source of power (ie Mana for Mages)
+		stats.set(4, 10); //Attack
+		stats.set(5, 10); //Special
+		stats.set(6, 10); //Dexterity
+		stats.set(7, 10); //Miscellaneous stat 
+	}
+	public int getHP(){
+		return stats.get(0);
+	}
+	public void changeHP(int change){
+		if (stats.get(1) + change > stats.get(0)){
+			stats.set(1, stats.get(0));
+		}
+		else if (stats.get(1) + change < 0){
+			stats.set(1, 0);
+		}
+		else {
+			stats.set(1, stats.get(1) + change);
+		}
+	}
+	public void changeEnergy(int change){
+		if (stats.get(3) + change > stats.get(2)){
+			stats.set(3, stats.get(2));
+		}
+		else if (stats.get(3) + change < 0){
+			stats.set(3, 0);
+		}
+		else {
+			stats.set(3, stats.get(3) + change);
+		}
+	}
+	public int getAttack(){
+		return stats.get(4);
+	}
+	public int getSpecial(){
+		return stats.get(5);
+	}
+	public int getDexterity(){
+		return stats.get(6);
+	}
 }
