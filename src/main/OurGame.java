635877bf3641c442e3569aed923690b5e2bb633f
E Stringham <backfrip@users.noreply.github.com>
2015-06-14 13:34:25 -0400

    Theme persistence

--- src/main/OurGame.java
+++ src/main/OurGame.java
@@ -1,10 +1,12 @@
 package main;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import screen.*;
-
 import object.Inventory;
 import object.Stats;
 
@@ -13,12 +15,15 @@ public class OurGame extends Game {
     public static final Sprite playerSprite = null;
     public static final Stats playerStats = null;
     public static final Inventory playerInventory = null;
+    public static Music backgroundTheme;
 
     @Override
     public void create() {
+	backgroundTheme = Gdx.audio.newMusic(new FileHandle(
+		"resource/music/music1.mp3"));
+	backgroundTheme.setLooping(true);
 	setScreen(new Intro(this));
-	//setScreen(new GameScreen(this));
-
+	// setScreen(new GameScreen(this));
     }
 
 }
--- src/screen/Intro.java
+++ src/screen/Intro.java
@@ -21,87 +21,86 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.badlogic.gdx.graphics.g2d.stbtt.*;
 import com.badlogic.gdx.graphics.g2d.freetype.*;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
-public class Intro implements Screen{
-	private OurGame game;
+public class Intro implements Screen {
+    private OurGame game;
     private ShapeRenderer sr;
     private BitmapFont text, title;
     private SpriteBatch batch;
     private SpriteCache cache;
     private Texture background;
-    private Music music;
     private FreeTypeFontGenerator generator;
     private FreeTypeFontGenerator generator2;
-	private FreeTypeFontParameter parameter;
-	private FreeTypeFontParameter parameter2;
-	
-	public Intro(OurGame gameRef){
-		game = gameRef;
-				sr = new ShapeRenderer();
-		batch = new SpriteBatch();
-		cache = new SpriteCache();
-		text = new BitmapFont();
-		title = new BitmapFont();
-		background = new Texture(new FileHandle("resource/background/introduction.jpg"));
-		generator = new FreeTypeFontGenerator(Gdx.files.internal("resource/fonts/Olde English Regular.ttf"));
-        generator2 = new FreeTypeFontGenerator(Gdx.files.internal("resource/fonts/Tangerine_Regular.ttf"));
-		parameter = new FreeTypeFontParameter();
-		parameter2 = new FreeTypeFontParameter();
-		parameter.size = 250;
-		title = generator.generateFont(parameter); 
-		parameter2.size = 42;
-		text = generator2.generateFont(parameter2); 
-		music = Gdx.audio.newMusic(Gdx.files.internal("resource/music/music1.mp3"));
-	}
-	
-	
-    @Override
-	public void render(float delta){
-    	Gdx.gl.glClearColor(1, 1, 1, 1);
-    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    	music.play();
-    	batch.begin();
+    private FreeTypeFontParameter parameter;
+    private FreeTypeFontParameter parameter2;
 
-        //batch.draw(image, 250, 200);
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-    	//text.setColor(0.7f, 0.2f, 0.2f, 1);
-        title.setColor(Color.MAROON);
-        title.draw(batch, "Origin", 375, 700);
-        text.draw(batch, "Click Anywhere to Start", 500, 175);
-        batch.end();
-    	if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-    		music.stop();
-    		dispose();
-    		game.setScreen(new GameScreen(game));
-    	}
-    	if (Gdx.input.isKeyJustPressed(Keys.ESCAPE))
-    	    Gdx.app.exit();
-    	if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
-    	    if (!Gdx.graphics.isFullscreen()) {
-    		Gdx.graphics.setDisplayMode(
-    			Gdx.graphics.getDesktopDisplayMode().width,
-    			Gdx.graphics.getDesktopDisplayMode().height, true);
-    		Gdx.input.setCursorCatched(true);
-    	    } else {
-    		Gdx.graphics.setDisplayMode(1280, 720, false);
-    		Gdx.input.setCursorCatched(false);
-    	    }
-    	}
+    public Intro(OurGame gameRef) {
+	game = gameRef;
+	sr = new ShapeRenderer();
+	batch = new SpriteBatch();
+	cache = new SpriteCache();
+	text = new BitmapFont();
+	title = new BitmapFont();
+	background = new Texture(new FileHandle(
+		"resource/background/introduction.jpg"));
+	generator = new FreeTypeFontGenerator(
+		Gdx.files.internal("resource/fonts/Olde English Regular.ttf"));
+	generator2 = new FreeTypeFontGenerator(
+		Gdx.files.internal("resource/fonts/Tangerine_Regular.ttf"));
+	parameter = new FreeTypeFontParameter();
+	parameter2 = new FreeTypeFontParameter();
+	parameter.size = 250;
+	title = generator.generateFont(parameter);
+	parameter2.size = 42;
+	text = generator2.generateFont(parameter2);
+    }
+
+
+    @Override
+    public void render(float delta) {
+	Gdx.gl.glClearColor(1, 1, 1, 1);
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	batch.begin();
+
+	// batch.draw(image, 250, 200);
+	batch.draw(background, 0, 0, Gdx.graphics.getWidth(),
+		Gdx.graphics.getHeight());
+	// text.setColor(0.7f, 0.2f, 0.2f, 1);
+	title.setColor(Color.MAROON);
+	title.draw(batch, "Origin", 375, 700);
+	text.draw(batch, "Click Anywhere to Start", 500, 175);
+	batch.end();
+	if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+	    // OurGame.backgroundTheme.play();
+	    dispose();
+	    game.setScreen(new GameScreen(game));
 	}
-	
+	if (Gdx.input.isKeyJustPressed(Keys.ESCAPE))
+	    Gdx.app.exit();
+	if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+	    if (!Gdx.graphics.isFullscreen()) {
+		Gdx.graphics.setDisplayMode(
+			Gdx.graphics.getDesktopDisplayMode().width,
+			Gdx.graphics.getDesktopDisplayMode().height, true);
+		Gdx.input.setCursorCatched(true);
+	    } else {
+		Gdx.graphics.setDisplayMode(1280, 720, false);
+		Gdx.input.setCursorCatched(false);
+	    }
+	}
+    }
+
     @Override
     public void show() {
-	// TODO Auto-generated method stub
-
+	OurGame.backgroundTheme.play();
     }
 
     @Override
     public void resize(int width, int height) {
-	//viewport.update(width, height);
-	//batch = new SpriteBatch();
+	// viewport.update(width, height);
+	// batch = new SpriteBatch();
     }
 
     @Override
@@ -124,13 +123,11 @@ public class Intro implements Screen{
 
     @Override
     public void dispose() {
-	game.dispose();
 	sr.dispose();
 	batch.dispose();
 	cache.dispose();
 	text.dispose();
 	generator.dispose();
 	generator2.dispose();
-	music.dispose();
     }
 }
